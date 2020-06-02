08e4614d45fac1f0bdebe583a486ea7344c381f5
Amarthya <amarthyaseshu2@gmail.com>
2020-06-02 23:06:44 +0530

    FSJ0320B006

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,16 @@
+package controller;
 
+import java.sql.SQLException;
+
+import dao.SkillDAO;
+//import model.Skill;
+
+public class Main{
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//		Skill skill=new  Skill(null, null);
+		SkillDAO skillDAO=new SkillDAO();
+		System.out.println("List of All Skills");
+		skillDAO.listAllSkills();
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,31 @@
+package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	
+	public List<Skill> listAllSkills () throws ClassNotFoundException, SQLException {
+
+		String sql="select * from PPLD3L3 ORDER BY skillname ASC";
+		
+		PreparedStatement ps=ConnectionManager.getConnection().prepareStatement(sql);
+		
+		ResultSet rs=ps.executeQuery();
+
+		
+		while(rs.next()) {
+			// Retrieve by column name and displaying
+			String skillname=rs.getString(3);
+			System.out.println(skillname);
+		}
+		ConnectionManager.getConnection().close();
+		return null;
+		
+	}
+}
